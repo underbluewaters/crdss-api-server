92e93d37c86e56c11e88c922f65dec20585cf76f
Chad Burt <chad@underbluewaters.net>
2024-11-05 09:45:20 -0800

    Set 15 minute cache-control header on /metadata

--- src/index.ts
+++ src/index.ts
@@ -122,6 +122,7 @@ app.get("/:v/mvt/:z/:x/:y", async (c, next) => {
 });
 
 app.get("/metadata", async (c) => {
+  c.header("Cache-Control", "public, max-age=900");
   return c.json(attributeData);
 });
 
